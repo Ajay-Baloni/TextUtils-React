d66a36151c576c16ca6d6d74b2ae07562c0d0740
ajaybaloni- <ajaybaloni01@gmail.com>
2023-08-07 11:27:02 +0530

    dark mode in nav bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
- 
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
 function App() {
+  const [mode, setMode] = useState("light"); //wheather the dark mode is enabled or not
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      // document.body.style.backgroundColor = "black";  extra
+    } else {
+      setMode("light");
+      //document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
-    <Navbar title="TextUtils" />
-    <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below"/>
-    </div>
-    </> 
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" />
+      </div>
+    </>
   );
 }
 
